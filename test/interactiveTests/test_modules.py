d01d3ee48dc860fbbb8512273c67172aef541351
LR <Studium.LR@web.de>
2025-07-10 15:20:27 +0200

    [refactor] rename interactive test files

--- test/interactiveTests/test_modules.py
+++ test/interactiveTests/test_modules.py
@@ -1,14 +1,14 @@
 from turret.pi_controller import PiController
 
-pi = PiController(18,19,2,3)
+controller = PiController(18, 19, 2, 3, verbose=True)
 
 try:
     while True:
         time1 = float(input("Enter time for charge (0.0-5.0): "))
         time2 = float(input("Enter time for load (0.0-1.0): "))
         if 0.0 <= time1 <= 5.0 and 0.0 <= time2 <= 1.0:
-            pi.charge(time1)
-            pi.load(time2)
+            controller.charge(time1)
+            controller.load(time2)
         else:
             print("Time must be between 0.0 and 5.0.")
 
@@ -16,4 +16,4 @@ except KeyboardInterrupt:
     print("\nExiting...")
 
 finally:
-    pi.stop()
+    controller.stop()
--- test/interactiveTests/test_servos.py
+++ test/interactiveTests/test_servos.py
@@ -1,13 +1,13 @@
 from turret.pi_controller import PiController
 
-pi = PiController(18, 19, 2, 3)
+controller = PiController(18, 19, 2, 3, verbose=True)
 
 try:
     while True:
         angle1 = int(input("Enter angle for Servo 1 (0-180): "))
         angle2 = int(input("Enter angle for Servo 2 (60-120): "))
         if 0 <= angle1 <= 180 and 0 <= angle2 <= 180:
-            pi.align(angle1, angle2)
+            controller.align(angle1, angle2)
         else:
             print("Angles must be between 0 and 180.")
 
@@ -15,4 +15,4 @@ except KeyboardInterrupt:
     print("\nExiting...")
 
 finally:
-    pi.stop()
+    controller.stop()
