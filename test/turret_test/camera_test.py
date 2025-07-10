d55f5ee54d494f56230b5c757ad9b92fbe610f56
LR <Studium.LR@web.de>
2025-07-10 20:13:14 +0200

    [fix] resolved issue https://github.com/LR-gith/NerfTurret/issues/8#issue-3219439745
    Added tests for camera.py

---
+++ test/turret_test/camera_test.py
@@ -0,0 +1,98 @@
+import numpy as np
+import pytest
+
+from src.exception.exception import CameraException
+from src.turret.camera import Camera
+
+
+def test_init_success(mocker):
+    mock_cap = mocker.MagicMock()
+    mock_cap.read.return_value = (True, "mock_frame")
+    mock_cap.isOpened.return_value = True
+    mocker.patch("turret.camera.cv2.VideoCapture", return_value=mock_cap)
+    camera = Camera()
+
+    assert camera.frame == "mock_frame"
+    assert camera.thread.is_alive()
+
+    camera.running = False
+    camera.thread.join()
+
+
+def test_init_read_failed(mocker):
+    mock_cap = mocker.MagicMock()
+    mock_cap.read.return_value = (False, "mock_frame")
+    mocker.patch("turret.camera.cv2.VideoCapture", return_value=mock_cap)
+
+    with pytest.raises(CameraException):
+        Camera()
+
+
+def test_init_isOpened_failed(mocker):
+    mock_cap = mocker.MagicMock()
+    mock_cap.read.return_value = (True, "mock_frame")
+    mock_cap.isOpened.return_value = False
+    mocker.patch("turret.camera.cv2.VideoCapture", return_value=mock_cap)
+
+    with pytest.raises(CameraException):
+        Camera()
+
+
+def test_update_success(mocker):
+    mock_cap = mocker.MagicMock()
+    mock_cap.isOpened.return_value = True
+    mock_cap.read.side_effect = [
+        (True, "initial"),
+        (True, "frame")
+    ]
+    mocker.patch("turret.camera.cv2.VideoCapture", return_value=mock_cap)
+    camera = Camera()
+
+    with camera.lock:
+        assert camera.frame in ["initial", "frame"]
+
+    camera.running = False
+    camera.thread.join()
+
+
+def test_update_not_running(mocker):
+    mock_cap = mocker.MagicMock()
+    mock_cap.isOpened.return_value = True
+    mock_cap.read.side_effect = [(True, "initial"), (True, "frame")]
+    mocker.patch("turret.camera.cv2.VideoCapture", return_value=mock_cap)
+    thread_mock = mocker.MagicMock()
+    mocker.patch("turret.camera.threading.Thread", return_value=thread_mock)
+    camera = Camera()
+    camera.running = False
+    camera.update()
+
+    mock_cap.read.assert_called_once()
+    assert camera.frame == "initial"
+
+
+def test_read(mocker):
+    mock_cap = mocker.MagicMock()
+    mock_cap.isOpened.return_value = True
+    test_frame = np.array([[[0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0]]])
+    mock_cap.read.side_effect = [(True, test_frame),
+                                 (True, "frame")]
+    mocker.patch("turret.camera.cv2.VideoCapture", return_value=mock_cap)
+    thread_mock = mocker.MagicMock()
+    mocker.patch("turret.camera.threading.Thread", return_value=thread_mock)
+    camera = Camera()
+    ret, frame = camera.read()
+
+    assert (camera.frame == frame).all()
+    assert camera.ret == ret
+
+
+def test_stop(mocker):
+    mock_cap = mocker.MagicMock()
+    mock_cap.isOpened.return_value = True
+    mock_cap.read.side_effect = [(True, "initial"), (True, "frame")]
+    mocker.patch("turret.camera.cv2.VideoCapture", return_value=mock_cap)
+    camera = Camera()
+    camera.stop()
+
+    assert not camera.thread.is_alive()
+    mock_cap.release.assert_called_once()
